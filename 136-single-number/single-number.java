e5e1c9bcfd3868451ab250072cf571531f1bb959
Suraj Patel <surajpatel2512006@gmail.com>
2026-07-17 17:20:07 +0530

    Time: 1 ms (99.97%) | Memory: 47 MB (28.03%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int sum = 0;
         for (int num : nums){
-            sum = sum^num;
+            sum = sum ^ num;
         }
         return sum;
         
